34db7aa3dea2ecd5aa933dd4540cc17e540f9758
faeruumi <faeruumi@gmail.com>
2026-08-24 01:50:58 +0800

    Create activate.js

---
+++ commands/general/activate.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('activate')
+        .setDescription('activate your matching cards!'),
+        
+    async execute(interaction) {
+        
+        await interaction.deferReply();
+        try {
+            const choices = [
+                'A',
+                'B',
+                'C',
+            ];
+            const size = choices.length;
+
+            const final = Math.floor(Math.random() * size) + 1; 
+
+            const embed = new EmbedBuilder()
+                .setTitle(`✧ ${choices[final-1]}`)
+                .setColor(0xEBBCA2)
+                .setDescription(`${choices[final-1]}`);
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (err) {
+            await interaction.editReply(`Error: ${err.message}`);
+        }
+    },
+};
