5a884575937903a4554e725ec8c8a683b90c8ae2
faeruumi <faeruumi@gmail.com>
2026-08-24 15:22:12 +0800

    Update cards.js

--- commands/general/cards.js
+++ commands/general/cards.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder, EmbedBuilder } = require('discord.js');
 const { createCanvas, loadImage } = require('canvas');
 
 module.exports = {
